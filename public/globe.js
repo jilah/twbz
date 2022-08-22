1cf19174b760837f322f953c3926734cb5a9acd4
Jilah <111175989+jilah@users.noreply.github.com>
2022-08-22 16:21:15 +0700

    Update globe.js

--- public/globe.js
+++ public/globe.js
@@ -3,7 +3,7 @@ const countEl = document.getElementById('count');
 updateVisitCount();
 
 function updateVisitCount() {
-	fetch('https://api.countapi.xyz/hit/nge.cyclic.app/?amount=1')
+	fetch('https://api.countapi.xyz/hit/swapnilsparsh/?amount=1')
 	.then(res => res.json())
 	.then(res => {
 		countEl.innerHTML = res.value;
